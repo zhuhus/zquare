f6b2d70a7a1c1591c639564cb2cbe33b364f8290
zhuhus <112484324+zhuhus@users.noreply.github.com>
2025-05-13 19:40:02 +0800

    three

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,6 +2,6 @@ import {makeInstaller} from '@zquare/utils'
 import components from './components'
 
 const installer =makeInstaller(components)
-
+import '@zquare/theme/index.css'
 export * from '@zquare/components'
 export default installer
